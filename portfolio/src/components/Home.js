7c08b878f9a8b64ec511e26c4c55dc16ab7b0295
SayanH-BIT <sayan.haldar.brainium@gmail.com>
2024-12-09 19:19:10 +0530

    updating...

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // import './Home.css';
 
 const Home = () => {
   return (
     <section className="home">
       <div className="home-content">
-        <h1>Hello, I'm [Your Name]</h1>
+        <h1>Namaste, I'm Sayan Haldar</h1>
         <p>Welcome to my portfolio website!</p>
         <button className="btn">
-          <a href="#about">Learn More</a>
+          <Link to='/about'>Learn More</Link>
         </button>
       </div>
     </section>
